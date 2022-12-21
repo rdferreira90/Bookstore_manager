a9d88130ef3f9bdf1cfd37da8c495528403d1fa5
Rodrigo Da Silva Ferreira <rodrigoferreira@brq.com>
2022-12-21 15:41:22 -0300

    Criando as classes de modelo Author e Book

---
+++ src/main/java/com/rodrigoferreira/bookstoremanager/entity/Author.java
@@ -0,0 +1,17 @@
+package com.rodrigoferreira.bookstoremanager.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+}
